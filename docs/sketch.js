1b109f5db2e0c3a23c932e10fb4c1aa3d2bb8674
Natalia bakakuri <64376039+theonomiMC@users.noreply.github.com>
2020-09-17 18:33:55 +0400

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -91,7 +91,7 @@ function draw() {
   if (timer < 15) {
     text('time: ' + timer, 10, 80);
   }
-  if (timer == 0 && score >= 40) {
+  if (timer == 0 && score >= 35) {
     if(life >= 0){
     push();
     textFont('cursive', 50);
@@ -122,7 +122,7 @@ function draw() {
     }
     
   }
-  if (timer == 0 && score < 40 ) {
+  if (timer == 0 && score < 35 ) {
 //--Score text---    
     push();
     textFont('cursive', 40);
